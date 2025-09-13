27529b945b10f9594f46c7bb6ec252f2e6c94f5f
Doheera-kosi <kupourevans99@gmail.com>
2025-09-13 07:29:25 -0700

    Updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
+  title: "SHSHUB",
+  // title: "Lama Dev School Management Dashboard",
   description: "Next.js School Management System",
 };
 
